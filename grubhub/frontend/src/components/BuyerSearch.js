b9d6faacb0790c712018c13fd7b71e9eac55349a
wamiquem <wamique.ansari@sjsu.edu>
2019-10-06 03:43:03 -0700

    add filter to buyer search page and code cleanup

--- grubhub/frontend/src/components/BuyerSearch.js
+++ grubhub/frontend/src/components/BuyerSearch.js
@@ -3,6 +3,7 @@ import {Link} from 'react-router-dom';
 import cookie from 'react-cookies';
 import {Redirect} from 'react-router';
 import Navbar from './Navbar';
+import menuImage from '../images/menu_default_image.png'
 import backendURL from '../urlconfig';
 
 //create the Navbar Component
@@ -15,10 +16,16 @@ class BuyerSearch extends Component {
         this.state = {
             firstName: "",
             restaurants: null,
-            menuItem: props.match.params.menuItem
+            menuItem: props.match.params.menuItem,
+            dropdown: false,
+            cuisines: null,
+            cuisineName: "",
+            filteredCuisine: "",
+            initialrestaurants: null
         }
         this.searchRestaurants = this.searchRestaurants.bind(this);
-        this.filterRestaurants = this.filterRestaurants.bind(this);
+        this.changeHandler = this.changeHandler.bind(this);
+
     }
 
     //get the first name of buyer from backend  
@@ -34,10 +41,11 @@ class BuyerSearch extends Component {
                 })
             })
             .catch(err => console.log(err));
+            this.state.menuItem = "";
             this.searchRestaurants();
         }
     }
-    
+
     searchRestaurants(){
         fetch(`${backendURL}/buyer/searchRestaurants/?menuItem=${this.state.menuItem}`,{
             credentials: 'include'
@@ -45,27 +53,41 @@ class BuyerSearch extends Component {
             .then(res => res.json())
             .then(data => {
                 this.setState({
-                    restaurants: data.row
+                    restaurants: data.row,
+                    initialrestaurants: data.row
                 })
             })
             .catch(err => console.log(err));
     }
 
-    filterRestaurants(){
-        fetch(`${backendURL}/buyer/filterRestaurants`,{
-            credentials: 'include'
+    changeHandler = (e) => {
+        this.setState({
+            [e.target.name] : e.target.value
+        })
+    }
+
+    handleEditChange = e => {
+        this.state.restaurants = this.state.initialrestaurants;
+        const filteredRestaurants = this.state.restaurants.filter(function (restaurant) {
+            return restaurant.cuisine === e.target.value;
+          });
+        if (e.target.value === 'Select Cuisine') {
+            this.setState({
+                restaurants: this.state.initialrestaurants
             })
-            .then(res => res.json())
-            .then(data => {
-                this.setState({
-                    restaurants: data.row
-                })
-            })
-            .catch(err => console.log(err));
+        } else {
+            this.setState({
+            restaurants: filteredRestaurants
+        })
+        }
+        
     }
 
     render(){
         const isRestaurants = this.state.restaurants;
+        const initialrestaurants = this.state.initialrestaurants;
+        const result = [];
+        const map = new Map();
         let redirectVar = null;
         if(!cookie.load('cookie')){
             redirectVar = <Redirect to= "/"/>
@@ -75,54 +97,75 @@ class BuyerSearch extends Component {
                 {redirectVar}
 
                 <Navbar firstName = {this.state.firstName} />
-                {/* <Navbar firstName = {"test"} /> */}
-
+                <div>
                 <div className="container">
-                    <div className="add-section-form">
+                    <div className="owner-order-list">
                         <div className="main-div">
                             <div className="panel">
-                            <h2 style= {{color:"red"}}>{this.state.message}</h2>
-                                <h4>Search Page</h4>
+                                <h4>Restaurants</h4>
                                 <hr/>
-                                <div>
-                                    <button onClick = {this.searchRestaurants} className="btn btn-primary">Find Restaurants                
-                                        {/* <Link to="/buyer/search">Search Restaurants</Link> */}
-                                    </button> 
+                                <div class="col-md-12">
+                                    <div class="col-md-6">
+                                        <input type="search" className="input" size="75"  height="100%" name = "menuItem" onChange = {this.changeHandler} placeholder="Pizza, Sushi, Biryani..."/>
+                                    </div>
+                                    <div class="col-md-4">
+                                        <button onClick = {this.searchRestaurants} className="btn btn-primary">Find Restaurants</button>
+                                    </div>
+                                    { initialrestaurants 
+                                            ?
+                                            this.state.initialrestaurants.map(restaurant => {
+                                                if(!map.has(restaurant.cuisine)){
+                                                    map.set(restaurant.cuisine, true);    // set any value to Map
+                                                        result.push({
+                                                            cuisine: restaurant.cuisine,
+                                                            id: restaurant.id,
+                                                            name: restaurant.name
+                                                    });
+                                                } 
+                                            })
+                                            : <tr>  </tr>
+                                    }
+                                    <div style = {{display:'flex'}}>
+                                            <div class="form-group">
+                                            <label>Filter:</label>
+                                            <select style = {{display:'inline', width:'auto'}} class="form-control" name="orderStatus"
+                                                onChange = {this.handleEditChange}>
+                                                <option selected>Select Cuisine</option>
+                                                { result
+                                                    ?
+                                                    result.map(cuisineData => {
+                                                        return (
+                                                            <option>{cuisineData.cuisine}</option>
+                                                     )
+                                                        })
+                                                    :
+                                                        <tr>  </tr>
+                                                    }
+                                            </select>
+                                            </div>
+                                    </div>
                                 </div>
+
                                 <div>
-                                <button onClick = {this.filterRestaurants} className="btn btn-primary">Filter Restaurants                
-                                    </button> 
-                                </div>
-                                {/* <li> Name of the restaruant :  {this.state.restaurantName} </li> */}
-                                <div>
-                                    <table  style= {{border: 10, width:500}}>
-                                        <thead>
-                                            <th>Name</th>
-                                            <th>City</th>
-                                            <th>Street</th>
-                                            <th>State</th>
-                                        </thead>
-                                        <tbody>
-                                        { isRestaurants 
+                                { isRestaurants 
                                             ?
                                             this.state.restaurants.map(restaurant => {
                                                 return (
-                                                    <tr>
-                                                    <td>{restaurant.name}</td>
-                                                    <td>{restaurant.city}</td>
-                                                    <td>{restaurant.street}</td>
-                                                    <td><Link to={`/buyer/place-order/${restaurant.id}`}>{restaurant.id}</Link></td>
-                                                    </tr>
+                                                    <div className="BuyerSearchList">
+                                                        <Link to={`/buyer/place-order/${restaurant.id}`}>
+                                                        <div> <img className="rounded float-left img-thumbnail" src = {menuImage} alt="Responsive image"></img>
+                                                            {restaurant.name} {restaurant.city} {restaurant.stree} {restaurant.state}</div>
+                                                        </Link>
+                                                    </div>
                                                 )
                                             })
                                             : <tr> NA </tr>
                                         }
-                                        </tbody>
-                                    </table>
                                 </div>
                             </div>
                         </div>
                     </div>
+                </div>
                 </div>
             </div>
         )
